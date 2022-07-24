7a437e8b898fdc5cca5639bdb814efad05e09837
Adam Proctor <aproctor@gmail.com>
2022-07-23 23:00:52 -0400

    bot formatting

--- bot.js
+++ bot.js
@@ -8,9 +8,55 @@ const REDIS_PORT = process.env.REDIS_PORT
 const DISCORD_TOKEN = process.env.DISCORD_TOKEN
 const KEYS_COMMAND = "!keys"
 
-const WHITE_LIST = ["Threatdown-Mal'Ganis", "Forbins-Mal'Ganis"];
+const WHITE_LIST = [
+    "Threatdown-Mal'Ganis",
+    "Forbins-Mal'Ganis",
+    "Proctorb-Mal'Ganis",
+    "Lelliana-Mal'Ganis",
+    "Adriatikis-Mal'Ganis",
+    "Wagsrogue-Mal'Ganis",
+    "Adamdd-Mal'Ganis",
+    "Rexaj-Mal'Ganis",
+    "Adamd-Mal'Ganis",
+    "Adamddh-Mal'Ganis",
+    "Yenstah-Mal'Ganis",
+    "Yensta-Mal'Ganis",
+    "Wagsdruid-Mal'Ganis",
+    "Wagsshaman-Mal'Ganis",
+    "Wagsmonk-Mal'Ganis",
+    "Moleboi-Mal'Ganis",
+    "Münkybrewstr-Mal'Ganis",
+    "Moleboye-Mal'Ganis",
+    "Moleboy-Mal'Ganis",
+    "Molebuoy-Mal'Ganis",
+    "Picklzz-Mal'Ganis",
+    "Foxmckloud-Mal'Ganis",
+    "Maggottron-Mal'Ganis",
+    "Tokiwarteeth-Mal'Ganis",
+    "Kriegerdots-Mal'Ganis",
+    "Saintcaine-Mal'Ganis",
+    "Gaelfury-Mal'Ganis",
+    "Holybabe-Mal'Ganis",
+    "Nomilk-Mal'Ganis",
+    "Dubert-Mal'Ganis",
+    "Holybabes-Mal'Ganis"];
 
 const DUNGEON_TABLE = {
+  "375": "Mists     ",
+  "376": "Necrotic  ",
+  "377": "DoS       ",
+  "378": "Halls     ",
+  "379": "Plaguefall",
+  "380": "Sanguiney ",
+  "381": "Spires    ",
+  "382": "Theater   ",
+  "391": "Streets   ",
+  "392": "Gambino   ",
+  "391F": "Streets   ",
+  "392F": "Gambino   "
+};
+
+const DUNGEON_TABLE_LONG = {
   "375": "Mists of Tirna Scithe",
   "376": "The Necrotic Wake",
   "377": "De Other Side",
@@ -25,6 +71,7 @@ const DUNGEON_TABLE = {
   "392F": "So'leah's Gambit"
 };
 
+let min_key_level = 15;
 
 let redis = new Redis({
   host: REDIS_HOST,
@@ -67,18 +114,23 @@ client.on('ready', () => {
         console.error(err);
       } else {
         let keyData = JSON.parse(result);
+        keyData.sort((a, b) => (a["key_level"] < b["key_level"]) ? 1 : -1);
+
         keyData.forEach(keyRecord => {
-          if(WHITE_LIST.includes(keyRecord["unit"])) {
-            console.log(keyRecord);
-            let friendString = `${keyRecord["unit"]} has a ${keyRecord["key_level"]} ${DUNGEON_TABLE[keyRecord["dungeon_id"]]}`;
-            console.log(friendString);
+          if(WHITE_LIST.includes(keyRecord["unit"]) && keyRecord["key_level"] >= min_key_level) {
+            //console.log(keyRecord);
+            let friendName = keyRecord["unit"].split('-')[0];
+            let friendString = `${keyRecord["key_level"]} ${DUNGEON_TABLE[keyRecord["dungeon_id"]]} ${friendName}`;
+            //console.log(friendString);
             friendData.push(friendString)
           }          
         });
       }
+
+      
       
       if(friendData.length > 0) {
-        message.channel.send(friendData.join('\n'));
+        message.channel.send(`\`\`\`\n${friendData.join('\n')}\n\`\`\``);
       } else {
         message.channel.send("No Keys found, please upload");
       }
