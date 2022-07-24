941b428bc82284c2ed2ec3cb257dd1dc90ccbeb0
adamderuwe <aderuwe@gmail.com>
2022-07-23 21:33:00 -0700

    Deleting docker start.  Not needed

--- start-redis.sh
+++
@@ -1,8 +0,0 @@
-docker image pull redislabs/redismod
-
-docker run \
-  -p 6379:6379 \
-  -v `pwd`/data:/data \
-  redislabs/redismod \
-  --loadmodule /usr/lib/redis/modules/redisbloom.so \
-  --dir /data
